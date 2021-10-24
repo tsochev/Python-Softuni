75cf047011c18aa4fe80b7986184f3cea94c4efb
Ivan Tsochev <ivantsochev@abv.bg>
2021-10-24 22:11:52 +0300

    Added solves

---
+++ Python Fundamentals/More Exercises/2 - Data Types and Variables/1. Exchange Integers.py
@@ -0,0 +1,13 @@
+first_num = int(input())
+second_num = int(input())
+
+temp = first_num
+a = second_num
+b = temp
+
+print("Before:")
+print(f"a = {first_num}")
+print(f"b = {second_num}")
+print("After:")
+print(f"a = {a}")
+print(f"b = {b}")
---
+++ Python Fundamentals/More Exercises/2 - Data Types and Variables/2. Prime Number Checker.py
@@ -0,0 +1,11 @@
+num = int(input())
+flag = False
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            flag = True
+            break
+if flag:
+    print("False")
+else:
+    print("True")
