a84af29a5d6a9201e7b3efd01c1072916b49cb5e
Ivan Tsochev <ivantsochev@abv.bg>
2022-07-11 23:03:37 +0300

    added solve

---
+++ Python OOP/Exercises/2. Classes And Objects/7. Album/project/band.py
@@ -0,0 +1,34 @@
+from project.album import Album
+
+
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album):
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name):
+        for album in self.albums:
+            if album.published:
+                return "Album has been published. It cannot be removed."
+
+            if album.name == album_name:
+                self.albums.remove(album)
+                return f"Album {album.name} has been removed."
+
+        return f"Album {album_name} is not found."
+
+    def details(self):
+        result = f"Band {self.name}"
+
+        for album in self.albums:
+            result += "\n"
+            result += album.details()
+
+        return result
